2d27b8dbb37a67d55a9f011a001644cd87b5c56f
maithanhtrong1113 <maithanhtrong1113@gmail.com>
2023-06-07 00:15:43 +0700

    change api

--- pages/index.js
+++ pages/index.js
@@ -25,7 +25,7 @@ const index = (props) => {
 export async function getServerSideProps() {
   // Fetch data from an API or perform any other async operations
   const response = await fetch(
-    "https://nhathuoclt99pharmacity.click/khach-hang/xem-thuoc/danh-sach-thuoc"
+    "https://khoaluanquanlynhathuoclt99.lol/khach-hang/xem-thuoc/danh-sach-thuoc"
   );
   const data = await response.json();
 
